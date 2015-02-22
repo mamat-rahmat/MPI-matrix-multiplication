b3dc474abdf0b4395bfce8f211bfbe73b1f96403
mamat-rahmat <coderbodoh@gmail.com>
2015-02-22 20:23:12 +0700

    mencoba nganggurin proses

--- matrix_mul.c
+++ matrix_mul.c
@@ -10,108 +10,111 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	int N = atoi(argv[1]);
-	
+
 	int *a = NULL;
 	int *b = NULL;
 	int *c = NULL;
 
 	int rank, size;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if(size > N)
+		size = N;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	int i, j, k;
-	double x_milliseconds=0.0;
-	double x_startTime,x_countTime;
-
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-	printf("Hello! from processor %s, task %d of %d, \n", processor_name, rank, size);
-	
-	if(rank == 0)
+	if(rank < N)
 	{
-		system("clear");
-		srand(time(NULL));
-		printf("ukuran matriks: %d\n", N);
-		MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		int i, j, k;
+		double x_milliseconds=0.0;
+		double x_startTime,x_countTime;
 
-		a = malloc(N*N*sizeof(int));
-		for(i=0; i<N; i++)
-			for(j=0; j<N; j++)
-				a[i*N + j] = (rand()%19)-9;
+		char processor_name[MPI_MAX_PROCESSOR_NAME];
+		int name_len;
+		MPI_Get_processor_name(processor_name, &name_len);
+		printf("Hello! from processor %s, task %d of %d, \n", processor_name, rank, size);
 		
-		b = malloc(N*N*sizeof(int));
-		for(i=0; i<N; i++)
-			for(j=0; j<N; j++)
-				b[i*N + j] = (rand()%19)-9;
-
-		c = malloc(N*N*sizeof(int));
-
-		printf("matriks A:\n");
-		for(i=0; i<N; i++)
+		if(rank == 0)
 		{
-			for(j=0; j<N; j++)
-				printf("%d ", a[(i*N)+j]);
+			system("clear");
+			srand(time(NULL));
+			printf("ukuran matriks: %d\n", N);
+			MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+			a = malloc(N*N*sizeof(int));
+			for(i=0; i<N; i++)
+				for(j=0; j<N; j++)
+					a[i*N + j] = (rand()%19)-9;
+			
+			b = malloc(N*N*sizeof(int));
+			for(i=0; i<N; i++)
+				for(j=0; j<N; j++)
+					b[i*N + j] = (rand()%19)-9;
+
+			c = malloc(N*N*sizeof(int));
+
+			printf("matriks A:\n");
+			for(i=0; i<N; i++)
+			{
+				for(j=0; j<N; j++)
+					printf("%d ", a[(i*N)+j]);
+				printf("\n");
+			}
 			printf("\n");
-		}
-		printf("\n");
-		printf("matriks B:\n");
-		for(i=0; i<N; i++)
-		{
-			for(j=0; j<N; j++)
-				printf("%d ", b[(i*N)+j]);
+			printf("matriks B:\n");
+			for(i=0; i<N; i++)
+			{
+				for(j=0; j<N; j++)
+					printf("%d ", b[(i*N)+j]);
+				printf("\n");
+			}
 			printf("\n");
+			x_startTime = MPI_Wtime();
 		}
-		printf("\n");
-		x_startTime = MPI_Wtime();
-	}
-	else
-	{
-		MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		b = malloc(N*N*sizeof(int));
-	}
-
-	//elements per process
-	int epp = (N*N)/size;
-	if ((N*N)%size!=0)	{
-		epp += 1;
-	}
-
-	// create a buffer that will hold the rows
-	int *row = malloc(sizeof(int) * epp);
-
-	MPI_Scatter(a, epp, MPI_INT, row, epp, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(b, N*N, MPI_INT, 0, MPI_COMM_WORLD);
-
-	// create a buffer that will hold the result
-	int *res = malloc(sizeof(int) * epp);
-	for(i=0; i<epp/N; i++)
-		for(j=0; j<N; j++)
+		else
 		{
-			res[i*N+j] = 0;
-			for(k=0; k<N; k++)
-				res[i*N+j] += row[i*N + k] * b[k*N + j];
+			MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			b = malloc(N*N*sizeof(int));
 		}
 
-	MPI_Gather(res, epp, MPI_INT, c, epp, MPI_INT, 0, MPI_COMM_WORLD);
 
-	//synchronize
-	MPI_Barrier(MPI_COMM_WORLD);
+		//elements per process
+		int epp = (N*N)/size;
+			
+		// create a buffer that will hold the rows
+		int *row = malloc(sizeof(int) * epp);
 
-	if(rank == 0)
-	{
-		x_countTime = MPI_Wtime();
-		x_milliseconds = (x_countTime-x_startTime)*1000;
-		printf("hasil perkalian matriks AxB:\n");
-		for(i=0; i<N; i++)
-		{
+		MPI_Scatter(a, epp, MPI_INT, row, epp, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(b, N*N, MPI_INT, 0, MPI_COMM_WORLD);
+
+		// create a buffer that will hold the result
+		int *res = malloc(sizeof(int) * epp);
+		for(i=0; i<epp/N; i++)
 			for(j=0; j<N; j++)
-				printf("%d ", c[(i*N)+j]);
-			printf("\n");
+			{
+				res[i*N+j] = 0;
+				for(k=0; k<N; k++)
+					res[i*N+j] += row[i*N + k] * b[k*N + j];
+			}
+
+		MPI_Gather(res, epp, MPI_INT, c, epp, MPI_INT, 0, MPI_COMM_WORLD);
+
+		//synchronize
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		if(rank == 0)
+		{
+			x_countTime = MPI_Wtime();
+			x_milliseconds = (x_countTime-x_startTime)*1000;
+			printf("hasil perkalian matriks AxB:\n");
+			for(i=0; i<N; i++)
+			{
+				for(j=0; j<N; j++)
+					printf("%d ", c[(i*N)+j]);
+				printf("\n");
+			}
+			printf("waktu perhitungan: %f milisecond(s)\n", x_milliseconds);
+			printf("jumlah elemen per proses: %d\n", epp);
 		}
-		printf("waktu perhitungan: %f milisecond(s)\n", x_milliseconds);
-		printf("jumlah elemen per proses: %d\n", epp);
 	}
 
 	MPI_Finalize();
